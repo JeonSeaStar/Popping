d9332ae7986794cb7b3944dfad6209dde5b36aa3
gwang jin <kflrm1234@naver.com>
2023-12-06 19:14:53 +0900

    Update BallonGenerator.cs

--- Assets/BallonGenerator.cs
+++ Assets/BallonGenerator.cs
@@ -26,7 +26,7 @@ public class BallonGenerator : MonoBehaviour
     {
         int i = Random.Range(0, spawnPointList.Count);
 
-        GameObject ballon = Instantiate(GetBallon(), spawnPointList[i] - new Vector3(0, -0.005f, 0), Quaternion.Euler(-90, 0, 0));
+        GameObject ballon = Instantiate(GetBallon(), spawnPointList[i] - new Vector3(0, -1.5f, 0), Quaternion.Euler(-90, 0, 0));
     }
 
     private GameObject GetBallon()
